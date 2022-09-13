0cfc5960e3acc22bc80a8dc557b1d1441643942f
SyarGekai <juanjo20-1998@outlook.com>
2022-09-13 01:47:37 -0600

    [DB Test 1]

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Test extends BaseController
 {
     public function index()
     {
-        return view('test');
+        return view('welcome_message');
     }
 }
